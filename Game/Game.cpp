30858ed785b6cb7489816037cdaf52867f2a69a3
alex-haa <ahalexha@gmail.com>
2023-05-26 11:19:30 -0700

    header and cpp file for Game object

---
+++ Game/Game.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+#include "Game.h"
+
+using namespace std;
+
+string Game::Game(name) { 
+    cout << "wip" << cout;
+}
+
+string Game::getName() {
+    return name;
+}
+
+void Game::play() {
+    cout << "wip" << endl;
+}
