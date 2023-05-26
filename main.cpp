21dc45e948ff952d2150f26f6381117a73c0363d
alex-haa <ahalexha@gmail.com>
2023-05-25 17:25:30 -0700

    updated main interface file

--- main.cpp
+++ main.cpp
@@ -11,20 +11,31 @@ void displayMenu();
 
 void displayMenu() {
     while (validChoice == false) {
-        cout << "Type 'Wordle' to play Wordle, 'Hangman' to play Hangman, or 'MQuiz' for Math Quizzes" << endl;
+        cout << "Game choices: " << endl;
+        cout << endl;
+        cout << "1. Wordle" << endl;
+        cout << "2. Math Quiz" << endl;
+        cout << "3. Hangman" << endl;
+        cout << "4. Quit" << endl;
+        cout << endl
+        cout << "Select an option by entering the respective number of the game" << endl;
         cin >> userChoice;
 
-        if (userChoice == "Wordle") {
+        if (userChoice == "1") {
             validChoice = true;
             play(Wordle);
         }
-        else if (userChoice == "Hangman") {
+        else if (userChoice == "2") {
             validChoice = true;
             play(Hangman);
         }
-        else if (userChoice == "MQuiz") {
+        else if (userChoice == "3") {
             validChoice = true;
             play(MQuiz);
+        }
+        else if (userChoice == "4") {
+            validChoice = true;
+            return;
         }
         else {
             cout << "Invalid option. Try again." << endl;
@@ -37,7 +48,7 @@ int main() {
     string userChoice;
     Game name;
 
-    cout << "Welcome to Amplify. Please enter the game you would like to play." << endl;
+    cout << "Welcome to Amplify " << name << endl;
 
     displayMenu();
 }
