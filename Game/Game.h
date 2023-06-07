556abee87fdde0f05c34a47780d222b9b2dc8b82
JarodPineapple <jarodlhendrickson@gmail.com>
2023-06-06 23:57:37 -0700

    Bug fixes

--- Game/Game.h
+++ Game/Game.h
@@ -1,18 +1,20 @@
 #ifndef GAME_H_
 #define GAME_H_
 
-#include <iostream>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
-class Game  {
-    public:
-        string Game(name);
-        string getName();
-        void play();
-    
-    private:
-        string name;
-        int highScore = 0;
-}
+class Game
+{
+	public:
+		string gameName(string name);
+		string getName();
+		void play();
+	private:
+		string name;
+		int highScore = 0;
+};
+
+#endif
--- Wordle/Wordle.headers/WordleMatch.h
+++ Wordle/Wordle.headers/WordleMatch.h
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 
-class WordleDisplay
+class WordleMatch
 {   
     public:
         string getRandomWord();
