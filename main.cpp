516ce4ace8f9171edd3a6e67c4872dc05371264f
JarodPineapple <jarodlhendrickson@gmail.com>
2023-06-09 00:54:37 -0700

    Fixes for Game.h and main.cpp

--- main.cpp
+++ main.cpp
@@ -1,53 +1,66 @@
-#include <string>
 #include <iostream>
+#include <string>
 #include <stdlib.h>
 
-#include "Wordle.h"
-#include "Hangman.h"
-#include "MathQuiz.h"
-#include "Game.h"
+#include "Game/Game.h"
 
 using namespace std;
 
-int main() {
-    string input;
-    string userChoice;
-    Game name;
+int main()
+{
+	int flag = 0;
+	int userInput = 0;
+	string userName = "no username";
+	
+	cout << "Enter your name: " << endl;
+	cin >> userName;
+	
+	while (flag == 0)
+	{
+		system("clear");
+		
+		cout << "Welcome to Amplify " << userName << " !" << endl << endl;
+		cout << "Game Choices:" << endl << endl;
+		cout << "1. Wordle" << endl;
+		cout << "2. Math Quiz" << endl;
+		cout << "3. Hangman" << endl;
+		cout << "4. Quit" << endl << endl << endl;
+		cout << "Type the number of the option you would like to choose, followed by the enter key: ";
+		cin >> userInput;
+		
+		flag = 1;
 
-    cout << "Enter your name: " << endl;
-    cin >> input;
-    name.username(string input);
-    system("clear");
+		while (flag == 1)
+		{
+			if (userInput == 1)
+			{
+				WordleGame wordle;
+				wordle.startGame();
+				flag = 0;
+			}
+			else if (userInput == 2)
+			{
+				MathQuizGame mathquiz;
+				mathquiz.startGame();
+				flag = 0;
+			}
+			else if (userInput == 3)
+			{
+				HangmanGame hangman;
+				hangman.startGame();
+				flag = 0;
+			}
+			else if (userInput == 4)
+			{
+				exit(1);
+			}
+			else
+			{
+				cout << "That is not one of the options. Please enter one of the four options listed above, followed by the enter key: ";
+				cin >> userInput;
+			}
+		}
+	}
 
-    cout << "Welcome to Amplify " << getName() << " !" << endl;
-    cout << endl;
-    cout << "Game Choices:" << endl;
-    cout << endl;
-    cout << "1. Wordle" << endl;
-    cout << "2. Math Quiz" << endl;
-    cout << "3. Hangman" << endl;
-    cout << "4. Quit" << endl;
-    cout << endl;
-    cout << endl;
-    cout << "Select an option:";
-    cin >> userChoice;
-
-    if (userChoice == "Wordle") 
-    {
-        startWordle();
-    }
-    else if (userChoice == "Math Quiz") 
-    {
-        startGame();
-    }
-    else if (userChoice == "Hangman") 
-    {
-        startHangman();
-    }
-    else if (userChoice == "Quit") 
-    {
-        exit(1);
-    }
-
-    return 0;
+	return 0;
 }
